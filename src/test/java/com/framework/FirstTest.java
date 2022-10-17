a0887e0b4677ca22f2bc8140779bd7b7be9170eb
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-17 15:07:14 -0400

    first test will pass and fail

---
+++ src/test/java/com/framework/FirstTest.java
@@ -0,0 +1,17 @@
+package com.framework;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FirstTest {
+
+    @Test
+    void FirstTest() {
+        int result = sum(2, 2);
+
+        Assertions.assertEquals(4, result);
+    }
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
--- src/test/java/com/framework/Test.java
+++
@@ -1,7 +0,0 @@
-package com.framework;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("this is working");
-    }
-}
