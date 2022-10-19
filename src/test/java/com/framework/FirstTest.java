a4c2e84e332ad647ffce6978f341d9150d9740d5
Rina Mendoza <rinamendozaa@gmail.com>
2022-10-18 20:15:55 -0400

    arrange of the first test set up

--- src/test/java/com/framework/FirstTest.java
+++ src/test/java/com/framework/FirstTest.java
@@ -2,14 +2,30 @@ package com.framework;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class FirstTest {
 
     @Test
-    void FirstTest() {
-        int result = sum(2, 2);
+    void usernameIsCorrectOverviewTab() {
+        //Arrange
+        System.setProperty("webdriver.chrome.driver", "/Users/rinamendoza/Downloads/chromedriver");
+        ChromeOptions options = new ChromeOptions().addArguments("start fullscreen");
 
-        Assertions.assertEquals(4, result);
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
+        String user = "mendo94";
+        driver.get("https://github.com/" + user);
+
+
+        // Act
+
+        // Assert
     }
     static int sum(int a, int b) {
         return a + b;
